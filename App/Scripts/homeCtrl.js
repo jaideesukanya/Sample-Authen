74344f35d625a163c2428ef9991c9d769842196d
boszkl8er <kittipong.iti17@gmail.com>
2017-10-09 17:39:51 +0700

    d

--- App/Scripts/homeCtrl.js
+++ App/Scripts/homeCtrl.js
@@ -10,12 +10,13 @@ angular.module('catApp')
     $scope.isActive = function (viewLocation) {        
         return viewLocation === $location.path();
     };
-	/*$scope.getToken2 = function(resource){
+	$scope.getToken2 = function(resource){
 		 //adalService.getToken22();
 		 //console.log('Call getCachedToken = '+adalService.getCachedToken('/App/Views/Home.html'));
 		 
 		 //eb4c0c67-7376-4a50-b9fe-ed01fe020b58
-		 console.log('Call homeCtrl getCachedToken = '+adalService.getCachedToken(resource));
-		 return adalService.getCachedToken('/App/Views/Home.html');
-	};*/
+		 var tokenVal = adalService.getCachedToken(resource));
+		 console.log('Call homeCtrl getCachedToken = '+tokenVal);
+		 return tokenVal;
+	}';
 }]);
