bd99aa559f13ca923c3a9167fbb53a71082998ba
boszkl8er <kittipong.iti17@gmail.com>
2017-10-10 13:07:42 +0700

    Update code

--- App/Scripts/homeCtrl.js
+++ App/Scripts/homeCtrl.js
@@ -10,7 +10,8 @@ angular.module('catApp')
     $scope.isActive = function (viewLocation) {        
         return viewLocation === $location.path();
     };
-	$scope.getToken2 = function(resource){
+	$scope.getToken2 = function(){
+		 var resource = 'eb4c0c67-7376-4a50-b9fe-ed01fe020b58';
 		 var tokenVal = adalService.getCachedToken(resource);
 		 return tokenVal;
 	};
