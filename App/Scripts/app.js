c67aff3219e9cb6d594c8102c713c284d7cb41bc
jaideesukanya <s.jaidee@togetherteam.co.th>
2017-10-10 14:23:37 +0700

    edit client id

--- App/Scripts/app.js
+++ App/Scripts/app.js
@@ -24,9 +24,9 @@ angular.module('catApp', ['ngRoute','AdalAngular'])
             tenant: 'together.at',
 			//tenant: '3e0b6cbd-4959-4d01-81bf-ce883ddabd96',
 			//clientId: '0a07a77d-ee2e-477e-93ca-fd7f59421237', Old clientID
-			
+			 clientId: '84a30349-7c6d-42fc-a92e-fa7b774f66e9',
 			//My clientID
-			clientId: 'eb4c0c67-7376-4a50-b9fe-ed01fe020b58',
+			//clientId: 'eb4c0c67-7376-4a50-b9fe-ed01fe020b58',
 			
 			//clientId: '5829dae7-8f84-470a-b11c-6cf32545f074',
             extraQueryParameter: 'nux=1',
